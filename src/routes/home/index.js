80c7e71538350b215a2f6e63f67f3f93977aea47
sergeysatori <38408406+sergeysatori@users.noreply.github.com>
2018-11-21 19:10:49 -0300

    mensaje de bajo construccion

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,7 +8,9 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={`${style.home} page`}>
-			 
+			 <h1>El sitio no está pronto aún! :o </h1>
+			 <p>Estamos construyendo éste sitio para que puedas acceder a <i><b>La Vida-la</b></i> desde cualquier lugar.<br/>
+			 Gracias por venir, te esperamos pronto!</p>
 				<div class={style.laVidaLaIntro}>
 
 				</div>
