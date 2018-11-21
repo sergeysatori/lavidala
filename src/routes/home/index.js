6438f51e3953f5a6cdcacc77fa866c67503eea59
sergeysatori <38408406+sergeysatori@users.noreply.github.com>
2018-11-21 19:18:46 -0300

    update message

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={`${style.home} page`}>
-			 <h1>El sitio no está pronto aún! :o </h1>
+			 <h1>El sitio no está listo aún! :o </h1>
 			 <p>Estamos construyendo éste sitio para que puedas acceder a <i><b>La Vida-la</b></i> desde cualquier lugar.<br/>
 			 Gracias por venir, esperamos que vuelvas pronto!</p>
 				<div class={style.laVidaLaIntro}>
