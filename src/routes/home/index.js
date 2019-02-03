d1eea0cf80e94c869dae31f9fa60b7dee2db6529
sergeysatori <38408406+sergeysatori@users.noreply.github.com>
2019-02-02 21:29:17 -0300

    tuten

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -10,33 +10,19 @@ import {getImageSize} from '../../utils/responsive'
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.handleResize = this.handleResize.bind(this)
+    // this.handleResize = this.handleResize.bind(this)
     // this.getImageSize = this.getImageSize.bind(this)
 
     this.state = {
-      imageSize: getImageSize(window.innerWidth, 1280, window.innerHeight, 960),
-      isMobile: isMobile()
+      // imageSize: getImageSize(document.body.clientWidth, 1280, document.body.clientHeight, 960),
+      // isMobile: isMobile()
     }
 
   }
-
-  componentWillMount() {
-    this.handleResize();
-  }
-  componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleResize);
-  }
-  handleResize() {
-
-    this.setState({
-      imageSize: getImageSize(document.body.clientWidth, 1280, window.innerHeight, 2000),
-      isMobile: isMobile()
-    })
-
-  }
+  //
+  // componentWillMount() {
+  //   this.handleResize();
+  // }
 
   // <div class={`${style.home} page`}>
   // </div>
@@ -51,30 +37,31 @@ export default class Home extends Component {
         overflow: 'hidden'
       }}>
 
-      <div name='crisalidas' style={{
-          width: `${this.state.imageSize.width}px`,
-          height: `${this.state.imageSize.height}px`,
-          marginTop: `${this.state.isMobile
-            ? '16vh'
-            : '0vh'}`,
+      <div name='crisalidas' class={style.crisalidas} style={{
+          // width: `${this.state.imageSize.width}px`,
+          // height: `${this.state.imageSize.height}px`,
+          // marginTop: `${this.state.isMobile
+          //   ? '16vh'
+          //   : '0vh'}`,
           backgroundImage: 'url(/assets/images/crisalidas.gif)',
           // backgroundSize: 'cover',
-          backgroundSize: `${this.state.imageSize.width}px ${this.state.imageSize.height}px`,
+          // backgroundSize: `${this.state.imageSize.width}px ${this.state.imageSize.height}px`,
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center top',
           margin: `2rem auto auto`
         }}>
 
         <div style={{
-            width: `${this.state.imageSize.width}px`,
-            // width: '100vw',
-            height: `${this.state.imageSize.height}px`,
+            // width: `${this.state.imageSize.width}px`,
+            width: '100%',
+            height: '100%',
+            // height: `${this.state.imageSize.height}px`,
             // maxWidth: '90vh',
             // backgroundImage: 'url(/assets/images/crisalidas.png)',
             // backgroundSize: 'contain',
             // backgroundRepeat: 'no-repeat',
             // backgroundPosition: 'top center',
-            // backgroundColor: '#b09f73',
+            // backgroundColor: 'hsla(43, 27%, 57%, 0.17)',
             position: 'relative',
             fontFamily: 'Sign Painter',
             writingMode: 'vertical-rl',
@@ -82,70 +69,71 @@ export default class Home extends Component {
             fontSize: '1.5rem',
             textTransform: 'uppercase'
           }}>
-          <div href="/canciones" onClick={() => route('/canciones')} title="Canciones" class={`${style.portadaLink}`} style={{
+          <div class={style.verticalLink} href="/canciones" onClick={() => route('/canciones')} title="Canciones"
+            style={{
               left: '5%',
-              top: `${this.state.isMobile
-                ? '0%'
-                : '16%'}`,
+              // top: `${this.state.isMobile
+              //   ? '0%'
+              //   : '16%'}`,
               width: '14.06%',
-              height: `${this.state.isMobile
-                ? '100%'
-                : '122vh'}`
+              // height: `${this.state.isMobile
+              //   ? '100%'
+              //   : '122vh'}`
             }}></div>
-          <div onClick={() => route('/obras-plasticas')} href="/obras-plasticas" title="Obras plásticas" class={`${style.portadaLink}`} style={{
+          <div onClick={() => route('/obras-plasticas')} href="/obras-plasticas" title="Obras plásticas" class={style.verticalLink} style={{
               left: '24%',
-              top: `${this.state.isMobile
-                ? '0%'
-                : '16%'}`,
+              // top: `${this.state.isMobile
+              //   ? '0%'
+              //   : '16%'}`,
               width: '14.06%',
-              height: `${this.state.isMobile
-                ? '100%'
-                : '122vh'}`
+              // height: `${this.state.isMobile
+              //   ? '100%'
+              //   : '122vh'}`
             }}></div>
-          <div onClick={() => route('/fotos-y-videos')} href="/fotos-y-videos" title="Fotos y vídeos" class={`${style.portadaLink}`} style={{
+          <div onClick={() => route('/fotos-y-videos')} href="/fotos-y-videos" title="Fotos y vídeos" class={style.verticalLink} style={{
               left: '42%',
-              top: `${this.state.isMobile
-                ? '0%'
-                : '16%'}`,
+              // top: `${this.state.isMobile
+              //   ? '0%'
+              //   : '16%'}`,
               width: '14.06%',
-              height: `${this.state.isMobile
-                ? '100%'
-                : '122vh'}`
+              // height: `${this.state.isMobile
+              //   ? '100%'
+              //   : '122vh'}`
             }}></div>
-          <div onClick={() => route('/material-tea')} href="/material-tea" title="Material TEA" class={`${style.portadaLink}`} style={{
+          <div onClick={() => route('/material-tea')} href="/material-tea" title="Material TEA" class={style.verticalLink} style={{
               left: '60%',
-              top: `${this.state.isMobile
-                ? '0%'
-                : '16%'}`,
+              // top: `${this.state.isMobile
+                // ? '0%'
+                // : '16%'}`,
               width: '14.06%',
-              height: `${this.state.isMobile
-                ? '100%'
-                : '122vh'}`
+              // height: `${this.state.isMobile
+                // ? '100%'
+                // : '122vh'}`
             }}></div>
-          <div onClick={() => route('/ficha-tecnica')} href="/ficha-tecnica" title="Ficha técnica" class={`${style.portadaLink}`} style={{
+          <div onClick={() => route('/ficha-tecnica')} href="/ficha-tecnica" title="Ficha técnica" class={style.verticalLink} style={{
               left: '80%',
-              top: `${this.state.isMobile
-                ? '0%'
-                : '16%'}`,
+              // top: `${this.state.isMobile
+                // ? '0%'
+                // : '16%'}`,
               width: '14.06%',
-              height: `${this.state.isMobile
-                ? '100%'
-                : '122vh'}`
+              // height: `${this.state.isMobile
+                // ? '100%'
+                // : '122vh'}`
             }}></div>
         </div>
       </div>
     </div>);
   }
 }
-function isMobile() {
-  // debugger
-
-  if (window.innerWidth < 630) {
-    return true
-  }
-  return false
-
-}
+// function isMobile() {
+//   // debugger
+//
+//   if (document.body.clientWidth < 630) {
+//     return true
+//   }
+//   return false
+//
+// }
 // <Card style={{color:'hsl(230, 90%, 58%)', width: `${window.innerWidth > 679 ? this.state.imageSize.width+'px' : '90vw'}`, margin: 'auto', maxWidth: '90vh'}}>
 //   <h1>Sitio en construcción</h1>
 //   <Icon>event_busy</Icon>
