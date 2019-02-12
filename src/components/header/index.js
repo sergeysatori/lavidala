f86e5c7d8927f5a1f6738b78533a52ed1fe1a6b6
sergeysatori <38408406+sergeysatori@users.noreply.github.com>
2019-02-12 19:46:11 -0300

    GA integration

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,6 +11,9 @@ import 'preact-material-components/Drawer/style.css';
 import 'preact-material-components/List/style.css';
 import 'preact-material-components/TopAppBar/style.css';
 import {canciones as cancionesData} from '../../database';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-134439002-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 // import style from './style';
 
 export default class Header extends Component {
