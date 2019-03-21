3f66e677dc6e82846075c093948e0a4b465c0a0d
Sergey <sergeysatori@gmail.com>
2019-03-20 23:39:46 -0300

    improve collavoration message

--- src/routes/apoyan/index.js
+++ src/routes/apoyan/index.js
@@ -37,11 +37,14 @@ export default class Apoyan extends Component {
           color: '#fff',
 					padding: '1rem'
         }}>
-        <p style={{fontSize: '1.2rem',
-        textRendering: 'optimizeLegibility'}} ><i>
-        Gracias a tu apoyo podemos seguir mejorando para disfrutar más arte juntos.
-        </i></p>
-        <p style={{fontSize: '1.5rem', textAlign: 'right', fontFamily: "Sign Painter"}} >
+        <p style={{
+          fontSize: '1.2rem',
+          textRendering: 'optimizeLegibility',
+          margin: '3rem'
+        }} >
+        Participando con tu apoyo podemos seguir mejorando para disfrutar de más arte juntos.
+        </p>
+        <p style={{fontSize: '1.5rem', textAlign: 'right', fontFamily: "Sign Painter", margin: '3rem'}} >
           <bold>La Vida-la</bold> <br/>
         </p>
 
@@ -49,6 +52,15 @@ export default class Apoyan extends Component {
         dangerouslySetInnerHTML={{
           __html: `<a mp-mode="dftl" class="${style.colaborateButton}"
                    href="https://www.mercadopago.com/mlu/checkout/start?pref_id=293736760-7ceed2b8-4994-4625-a8f4-c62e8a0f75b1" name="MP-payButton" class='orange-tr-s-rn-none'>Promotor <small>$100</small></a>
+          <script type="text/javascript">
+          (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+          </script>`
+        }}>
+
+        </p>
+        <p
+        dangerouslySetInnerHTML={{
+          __html: `<a mp-mode="dftl" href="https://www.mercadopago.com/mlu/checkout/start?pref_id=293736760-59ba7314-a812-4f6a-b542-7cdc9f83969e" name="MP-payButton" class="${style.colaborateButton}">Patrocinador <small>$300</small></a>
           <script type="text/javascript">
           (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
           </script>`
