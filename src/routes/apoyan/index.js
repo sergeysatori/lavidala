c3d8940a524f6041d2ec56e9857c78e4bda595bb
Sergey <sergeysatori@gmail.com>
2019-03-20 23:21:54 -0300

    improve message

--- src/routes/apoyan/index.js
+++ src/routes/apoyan/index.js
@@ -37,8 +37,15 @@ export default class Apoyan extends Component {
           color: '#fff',
 					padding: '1rem'
         }}>
+        <p style={{fontSize: '1.2rem',
+        textRendering: 'optimizeLegibility'}} ><i>
+        Gracias a tu apoyo podemos seguir mejorando para disfrutar más arte juntos.
+        </i></p>
+        <p style={{fontSize: '1.5rem', textAlign: 'right', fontFamily: "Sign Painter"}} >
+          <bold>La Vida-la</bold> <br/>
+        </p>
 
-        <p style={{fontSize: 'larger'}}
+        <p
         dangerouslySetInnerHTML={{
           __html: `<a mp-mode="dftl" class="${style.colaborateButton}"
                    href="https://www.mercadopago.com/mlu/checkout/start?pref_id=293736760-7ceed2b8-4994-4625-a8f4-c62e8a0f75b1" name="MP-payButton" class='orange-tr-s-rn-none'>Promotor <small>$100</small></a>
