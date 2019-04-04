fdf0b8226418f80bdeb0beb8d0057e4744a6f054
Sergey <sergeysatori@gmail.com>
2019-04-04 20:35:10 -0300

    add fullscreen mode

--- src/components/app.js
+++ src/components/app.js
@@ -15,15 +15,18 @@ import MaterialTEA from 'async!../routes/material-tea';
 import ObraPlastica from 'async!../routes/obra-plastica';
 import ObrasPlasticas from 'async!../routes/obras-plasticas';
 import FotosYVideos from 'async!../routes/fotos-y-videos';
+import Fullscreen from "react-full-screen";
 
 export default class App extends Component {
 	constructor(props) {
     super(props);
 		this.state = {
 			currentUrl: '',
-			hadFirstLoad: false
+			hadFirstLoad: false,
+			isFull: false
 		};
 		this.setFirstLoad = this.setFirstLoad.bind(this);
+		this.goFull = this.goFull.bind(this);
 	}
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
@@ -40,10 +43,30 @@ export default class App extends Component {
 			hadFirstLoad: true
 		});
 	}
-
+	goFull(){
+		this.setState({ isFull: true });
+	}
 	render() {
 		return (
 			<div id="app">
+			{ this.state.isFull ? false :
+			<div onClick={this.goFull}
+			style={{position: 'fixed',
+			top: '10px',
+			right: '17px',
+			cursor: 'pointer',
+			color: 'rgba(175, 148, 116, 0.4)',
+			paddingTop:'.5rem',
+			border: '',
+			zIndex:'3000'}}>
+												<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 18 18"><path style={{fill: 'rgba(175, 148, 116, 0.6)'}} d="M4.5 11H3v4h4v-1.5H4.5V11zM3 7h1.5V4.5H7V3H3v4zm10.5 6.5H11V15h4v-4h-1.5v2.5zM11 3v1.5h2.5V7H15V3h-4z"/></svg>
+			</div>}
+			<Fullscreen
+				style={{	overflow: 'auto'}}
+          enabled={this.state.isFull}
+          onChange={isFull => this.setState({isFull})}
+        >
+				<div style={{ overflow:'auto', width: '100%', height: '100vh'}}>
 				<Header selectedRoute={this.state.currentUrl}/>
 				<Router onChange={this.handleRoute}>
 					<Home hadFirstLoad={this.state.hadFirstLoad} setFirstLoad={this.setFirstLoad} path="/" />
@@ -58,6 +81,8 @@ export default class App extends Component {
 					<Apoyan path="/apoyan/" />
           <NotFound default />
 				</Router>
+				</div>
+				</Fullscreen>
 			</div>
 		);
 	}
